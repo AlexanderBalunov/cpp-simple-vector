2161357b6ddd18ece89bdf6b07d49cdf0eab8709
Alexander <142852639+AlexanderBalunov@users.noreply.github.com>
2024-08-09 18:52:52 +0300

    array_ptr.h

--- simple-vector/array_ptr.h
+++ simple-vector/array_ptr.h
@@ -1,3 +1,76 @@
-// вставьте сюда ваш код для класса ArrayPtr
-// внесите в него изменения, 
-// которые позволят реализовать move-семантику
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <utility>
+
+template <typename Type>
+class ArrayPtr {
+public:
+    ArrayPtr() = default;
+
+    explicit ArrayPtr(size_t size) {
+        if (size == 0) {
+            raw_ptr_ = nullptr;
+        } else {
+            raw_ptr_  = new Type[size];
+        }
+    }
+
+    explicit ArrayPtr(Type* raw_ptr) noexcept 
+        : raw_ptr_(raw_ptr)
+    {
+    }
+
+    ArrayPtr(const ArrayPtr& other) = delete;
+    
+    ArrayPtr(ArrayPtr&& other) noexcept 
+        : raw_ptr_(std::exchange(other.raw_ptr_, nullptr))
+    {
+    }
+
+    ~ArrayPtr() {
+        delete[] raw_ptr_;
+    }
+
+    ArrayPtr& operator=(const ArrayPtr&) = delete;
+
+    ArrayPtr& operator=(ArrayPtr&& rhs) noexcept {
+        raw_ptr_ = std::exchange(rhs.raw_ptr_, nullptr);
+        return *this;
+    }
+
+    [[nodiscard]] Type* Release() noexcept {
+        Type* ex_array = raw_ptr_;
+        raw_ptr_ = nullptr;
+        return ex_array;
+    }
+
+    Type& operator[](size_t index) noexcept {
+        return raw_ptr_[index];
+    }
+
+    const Type& operator[](size_t index) const noexcept {
+        return raw_ptr_[index];
+    }
+
+    explicit operator bool() const {
+        return raw_ptr_ != nullptr;
+    }
+
+    Type* Get() const noexcept {
+        return raw_ptr_;
+    }
+
+    void swap(ArrayPtr& other) noexcept {
+        std::swap(other.raw_ptr_, raw_ptr_);
+    }
+    
+    void swap(ArrayPtr&& other) noexcept {
+        raw_ptr_ = std::exchange(other.raw_ptr_, nullptr);
+    }    
+
+private:
+    Type* raw_ptr_ = nullptr;
+};
