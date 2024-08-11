567f94d5feb15e2168962e2fa78922a08eb0eca0
Alexander <142852639+AlexanderBalunov@users.noreply.github.com>
2024-08-11 18:55:49 +0300

    Update array_ptr.h

--- simple-vector/array_ptr.h
+++ simple-vector/array_ptr.h
@@ -10,8 +10,9 @@ class ArrayPtr {
 public:
     ArrayPtr() = default;
 
-    explicit ArrayPtr(size_t size) {
-        raw_ptr_ = size == 0 ? nullptr : new Type[size];
+    explicit ArrayPtr(size_t size) 
+        : raw_ptr_(size == 0 ? nullptr : new Type[size])
+    {
     }
 
     explicit ArrayPtr(Type* raw_ptr) noexcept 
@@ -20,24 +21,12 @@ public:
     }
 
     ArrayPtr(const ArrayPtr& other) = delete;
-    
-    ArrayPtr(ArrayPtr&& other) noexcept 
-        : raw_ptr_(std::exchange(other.raw_ptr_, nullptr))
-    {
-    }
 
     ~ArrayPtr() {
         delete[] raw_ptr_;
     }
 
     ArrayPtr& operator=(const ArrayPtr&) = delete;
-
-    ArrayPtr& operator=(ArrayPtr&& rhs) noexcept {
-        if (this != &rhs) {
-            raw_ptr_ = std::exchange(rhs.raw_ptr_, nullptr);
-        }
-        return *this;
-    }
 
     [[nodiscard]] Type* Release() noexcept {
         Type* ex_array = raw_ptr_;
@@ -53,7 +42,7 @@ public:
         return raw_ptr_[index];
     }
 
-    explicit operator bool() const {
+    explicit operator bool() const noexcept {
         return raw_ptr_ != nullptr;
     }
 
@@ -63,11 +52,7 @@ public:
 
     void swap(ArrayPtr& other) noexcept {
         std::swap(raw_ptr_, other.raw_ptr_);
-    }
-    
-    void swap(ArrayPtr&& other) noexcept {
-        raw_ptr_ = std::exchange(other.raw_ptr_, nullptr);
-    }    
+    }  
 
 private:
     Type* raw_ptr_ = nullptr;
